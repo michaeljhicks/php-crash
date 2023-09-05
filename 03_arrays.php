9034f2cc291b5882f93b29df93498e3790b004ee
Michael Hicks <hicks.michael04@gmail.com>
2023-09-05 11:46:31 -0600

    Complete conditionals

--- 03_arrays.php
+++ 03_arrays.php
@@ -27,23 +27,23 @@ var_dump($numbers);
   Associative arrays allow us to use named keys to identify values.
 */
 
-// $colors = [
-//     1 => 'red',
-//     2 => 'green',
-//     3 => 'blue',
-// ];
+$colors = [
+    1 => 'red',
+    2 => 'green',
+    3 => 'blue',
+];
 
 // echo $colors[1];
 
 // Strings as keys
-// $hex = [
-//     'red' => '#f00',
-//     'green' => '#0f0',
-//     'blue' => '#00f',
-// ];
+$hex = [
+    'red' => '#f00',
+    'green' => '#0f0',
+    'blue' => '#00f',
+];
 
-// echo $hex['red'];
-// var_dump($hex);
+echo $hex['red'];
+var_dump($hex);
 
 /* ---- Multi-dimensional arrays ---- */
 
@@ -52,36 +52,36 @@ var_dump($numbers);
 */
 
 // Single person
-// $person1 = [
-//     'first_name' => 'Brad',
-//     'last_name' => 'Traversy',
-//     'email' => 'brad@gmail.com',
-// ];
+$person1 = [
+    'first_name' => 'Brad',
+    'last_name' => 'Traversy',
+    'email' => 'brad@gmail.com',
+];
 
 // Array of people
-// $people = [
-//     $person1, //   [...$person1]
-//     [
-//         'first_name' => 'John',
-//         'last_name' => 'Doe',
-//         'email' => 'john@gmail.com',
-//     ],
-//     [
-//         'first_name' => 'Jane',
-//         'last_name' => 'Doe',
-//         'email' => 'jane@gmail.com',
-//     ],
-// ];
+$people = [
+    $person1, //   [...$person1]
+    [
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'email' => 'john@gmail.com',
+    ],
+    [
+        'first_name' => 'Jane',
+        'last_name' => 'Doe',
+        'email' => 'jane@gmail.com',
+    ],
+];
 
-// var_dump($people);
+var_dump($people);
 
 // Accessing values in a multi-dimensional array
-// echo $people[0]['first_name'];
-// echo $people[2]['email'];
+echo $people[0]['first_name'];
+echo $people[2]['email'];
 
 // Encode to JSON
-// var_dump(json_encode($people));
+var_dump(json_encode($people));
 
 // Decode from JSON
-// $jsonobj = '{"first_name":"Brad","last_name": "Traversy","email":"brad@gmail.com"}';
-// var_dump(json_decode($jsonobj));
+$jsonobj = '{"first_name":"Brad","last_name": "Traversy","email":"brad@gmail.com"}';
+var_dump(json_decode($jsonobj));
