d7504be6219ffe367a819fc359b797b7b2493565
Michael Hicks <hicks.michael04@gmail.com>
2023-09-05 14:26:29 -0600

    Complete superglobals

--- 00_test.php
+++ 00_test.php
@@ -1,15 +1,5 @@
 <?php
-
-$numbers = range(1, 20);
-
-$new_numbers = array_map(function($number) {
-    return "Number $number";
-}, $numbers);
-
-$less_thank_10 = array_filter($numbers, fn($number) => $number < 11);
-// print_r($less_thank_10);
-
-$sum = array_reduce($numbers, fn($carry, $number) => $carry + $number);
-print_r($sum);
+$string2 = '<h1>Hello</h1>';
+echo htmlspecialchars($string2);
 
 ?>
